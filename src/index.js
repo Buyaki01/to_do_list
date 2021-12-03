b6d89b85c92b2dc8c70b88b14116e2b79e1e3416
Buyaki <rittahbuyaki@gmail.com>
2021-12-03 22:38:38 +0300

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -15,25 +15,21 @@ function component() {
             </button>`;
   element.appendChild(addToList);
   const itemArray = LocalStorage.getList();
-  console.log(itemArray);
   itemArray.forEach((item) => {
     const listItem = document.createElement('li');
     listItem.classList.add('items_created');
     listItem.innerHTML = `<input type="checkbox" class="check_box"> <p>${item.description}</p> <button>
                 <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
               </button>`;
-              const completed = listItem.querySelector('.check_box');
-              completed.checked = item.completed;
-              completed.addEventListener('change', (e) => ToDoList.changeStatus(item.index, e.target.checked));          
+    const completed = listItem.querySelector('.check_box');
+    completed.checked = item.completed;
+    completed.addEventListener('change', (e) => ToDoList.changeStatus(item.index, e.target.checked));
     element.appendChild(listItem);
   });
-  
-  const lengthArray = itemArray.length;
   const clearCompleted = document.createElement('button');
   clearCompleted.classList.add('clear_completed');
   clearCompleted.innerHTML = 'Clear all completed';
   element.appendChild(clearCompleted);
-  console.log(element);
   return element;
 }
 
