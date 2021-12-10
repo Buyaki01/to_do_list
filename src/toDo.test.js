d1497d273e6bcb45869ca6626d58cf4814e19e25
Mong'are <Mongaresams@gmail.com>
2021-12-10 13:25:37 +0300

    Add the test code for add and delete functions

--- src/toDo.test.js
+++ src/toDo.test.js
@@ -1,4 +1,16 @@
 const LocalStorage = require('./storage.js');
 const ToDoList = require('./toDo.js');
-const Tasks = require('./todolistitem.js');
+// const Tasks = require('./todolistitem.js');
 
+describe('Check toDo List functions ', () => {
+  test('check add function ', () => {
+    expect(ToDoList.addToForm('Sweep the house')).toEqual({ description: 'Sweep the house', index: 1, completed: false });
+    expect(ToDoList.addToForm('Wash the dishes')).toEqual({ description: 'Wash the dishes', index: 2, completed: false });
+  });
+
+  test('check delete function ', () => {
+    ToDoList.deleteItem(1);
+    const model = LocalStorage.getList();
+    expect(model.length).toEqual(1);
+  });
+});
