f460304d927040e9129807d1a2b94f95b60a5418
Mong'are <Mongaresams@gmail.com>
2021-12-10 13:45:15 +0300

    Add the completed tasks function  jest test

--- src/toDo.test.js
+++ src/toDo.test.js
@@ -6,6 +6,19 @@ describe('Check toDo List functions ', () => {
     expect(ToDoList.addToForm('Sweep the house')).toEqual({ description: 'Sweep the house', index: 1, completed: false });
     expect(ToDoList.addToForm('Wash the dishes')).toEqual({ description: 'Wash the dishes', index: 2, completed: false });
   });
+  test('check edit function ', () => {
+    ToDoList.editForm('Mop the house', 1);
+    const model = LocalStorage.getList();
+    const listItem = model.find((item) => item.index === 1);
+    expect(listItem.description).toEqual('Mop the house');
+  });
+
+  test('changes status of completed tasks', () => {
+    ToDoList.changeStatus(2, true);
+    const model = LocalStorage.getList();
+    const listItem = model.find((item) => item.index === 2);
+    expect(listItem.completed).toEqual(true);
+  });
 
   test('check delete function ', () => {
     ToDoList.deleteItem(1);
@@ -13,10 +26,9 @@ describe('Check toDo List functions ', () => {
     expect(model.length).toEqual(1);
   });
 
-  test('check edit function ', () => {
-    ToDoList.editForm('Mop the house', 1)
+  test('check clear all completed function ', () => {
+    ToDoList.clearAllCompleted();
     const model = LocalStorage.getList();
-    const listItem = model.find((item) => item.index === 1);
-    expect(listItem.description).toEqual('Mop the house');
+    expect(model.length).toEqual(0);
   });
 });
