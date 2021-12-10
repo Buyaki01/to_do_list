407f69efa02868161ad17044ce2a9408d029f7a7
Mong'are <Mongaresams@gmail.com>
2021-12-10 13:31:42 +0300

    Fix linter error

--- src/toDo.test.js
+++ src/toDo.test.js
@@ -1,6 +1,5 @@
 const LocalStorage = require('./storage.js');
 const ToDoList = require('./toDo.js');
-// const Tasks = require('./todolistitem.js');
 
 describe('Check toDo List functions ', () => {
   test('check add function ', () => {
